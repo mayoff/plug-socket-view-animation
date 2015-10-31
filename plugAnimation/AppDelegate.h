97b65189ad517cc43971c65eff23cb81e3b4bc18
Rob Mayoff <git@rob.dqd.com>
2015-10-31 16:26:09 -0500

    Initial Commit

---
+++ plugAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  plugAnimation
+//
+//  Created by Rob Mayoff on 10/31/15.
+//  Copyright © 2015 Rob Mayoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
