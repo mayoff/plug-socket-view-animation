e7e8f4bf5f2bd2a03fb2ca668dc6ce4e011bd8c7
Rob Mayoff <git@rob.dqd.com>
2015-10-31 17:00:25 -0500

    animate socket view

--- plugAnimation/ViewController.m
+++ plugAnimation/ViewController.m
@@ -8,20 +8,56 @@
 
 #import "ViewController.h"
 
+@interface UIView (recursiveDescription)
+- (NSString *)recursiveDescription;
+@end
+
 @interface ViewController ()
+
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *socketWidthConstraint;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *socketHeightConstraint;
+@property (strong, nonatomic) IBOutlet UIView *socketView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+
+    CADisplayLink *link = [CADisplayLink displayLinkWithTarget:self selector:@selector(displayLinkDidFire:)];
+//    [link addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)displayLinkDidFire:(CADisplayLink *)link {
+    if (self.socketView.layer.presentationLayer) {
+        NSLog(@"%@", self.socketView.layer.presentationLayer);
+    }
+}
+
+- (IBAction)bigButtonWasTapped:(id)sender {
+    [UIView animateWithDuration:10 animations:^{
+        self.socketWidthConstraint.constant = 320;
+        self.socketHeightConstraint.constant = 320;
+        [self.view layoutIfNeeded];
+    }];
+}
+
+- (IBAction)smallButtonWasTapped:(id)sender {
+    [UIView animateWithDuration:10 animations:^{
+        self.socketWidthConstraint.constant = 160;
+        self.socketHeightConstraint.constant = 160;
+        [self.view layoutIfNeeded];
+    }];
+}
+
+- (IBAction)checkWasTapped:(id)sender {
+//    NSLog(@"%@", self.view.recursiveDescription);
+    NSLog(@"current time = %.6f", CACurrentMediaTime());
+    for (NSString *key in self.socketView.layer.animationKeys) {
+        CABasicAnimation *animation = (CABasicAnimation *)[self.socketView.layer animationForKey:key];
+        NSLog(@"%@ %.6f %.6f", animation.keyPath, animation.beginTime, animation.timeOffset);
+    }
 }
 
 @end
